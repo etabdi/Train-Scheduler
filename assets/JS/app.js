537bcd3f27b4909e52757140d63d7950324a1d23
Abdu <etabdi@yahoo.com>
2019-08-11 21:51:59 -0500

    fire base and timer created

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,3 +1,75 @@
+
+
+    // Initialize Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyCG0ki5t2lWD58geujSLRXMnzwtBSWWhdw",
+    authDomain: "amen-8dc1b.firebaseapp.com",
+    databaseURL: "https://amen-8dc1b.firebaseio.com",
+    projectId: "amen-8dc1b",
+    storageBucket: "",
+    messagingSenderId: "980426164534",
+    appId: "1:980426164534:web:de92cb84178981b6"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+//Capture a variable to reference the database.
+var database=firebase.database();
+ var trainName="";
+ var destination="";
+ var firstTrain="";
+ var frequency=0;
+
+//Capture button click
+$("#add-train").on("click",function(event){
+event.preventDefault();
+
+    frequency=$("#Frequency").val().trim();
+    trainName = $("#train-name").val().trim();
+    destination=$("#destination").val().trim();
+     firstTrainTime=$("#firstTrain-time").val().trim();
+    frequency=$("#Frequency").val().trim();
+
+    var timeConverted=moment(firstTrainTime,"HH:mm").subtract(1,"years");
+    console.log(timeConverted)
+    var diffTime = moment().diff(timeConverted, "m");
+    console.log(diffTime)
+    var timeRemainder =diffTime % frequency ;
+    var tminutes= frequency - timeRemainder;
+    console.log(timeRemainder+"timeremainder");
+    var nextArriva = moment().add(tminutes, "m").format("hh:mm"); 
+
+   database.ref().push(
+    {
+        trainName:trainName,
+        destination:destination,
+        firstTrainTime:firstTrainTime,
+        frequance:frequency,
+        nextArriva:nextArriva,
+        minitsAway:tminutes,  
+        
+    });  
+     
+    $("form")[0].reset();
+
+    console.log(trainName);
+    console.log(destination);
+    console.log(firstTrainTime);
+    console.log(frequency);
+
+});
+
+
+
+       
+database.ref().on("child_added", function(childSnapshot) {
+    $("#td-add").append("<tr><td>"+childSnapshot.val().trainName
+    +"<td>"+childSnapshot.val().destination
+    +"<td>"+childSnapshot.val().frequance
+    +"<td>"+childSnapshot.val().nextArriva
+    +"<td>"+childSnapshot.val().minitsAway+"</td></tr>");
+});
+
+
 function showTime(){
     var date = new Date();
     var h = date.getHours(); // 0 - 23
@@ -23,7 +95,7 @@ function showTime(){
     document.getElementById("MyClockDisplay").textContent = time;
     
     setTimeout(showTime, 1000);
-    
+   
 }
-
+ 
 showTime();
